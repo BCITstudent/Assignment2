81a94e9aa27b7312f5ad30800850df9f0d55ba13
Andrew Yoon <yoonandrew416@gmail.com>
2022-11-08 15:02:51 -0800

    while and for

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+// for (let i = o; i < 1000000; i++){
+//     console.log(i);
+// }
+
+// for(let i = 0; i <= 10; i++) {
+//     console.log(Math.pow(2 **i))
+// }
+// let i = 0
+// while (i <= 10)
+// {
+//     console.log(Math.pow(2 **i))
+//     i++;
+// }
+do{
+let input = prompt ("What is your name?");
+console.log(Number(input));
+}
+while()
