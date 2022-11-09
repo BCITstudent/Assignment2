876adf4dd553902c9f0195cafbeb7d01cf49af65
Andrew Yoon <yoonandrew416@gmail.com>
2022-11-08 16:12:20 -0800

    Activity

--- scripts/script.js
+++ scripts/script.js
@@ -42,16 +42,44 @@ for (let item of arr)
     console.log(2 ** item);
 }
 
-const person = {
-    name:['Bob', 'Smith'],
-    age:32,
-    gender: 'male',
-    interests: ['music', 'skiing']
-};
-console.log(`My name is ${person.name[0]} ${person.name[1]}. I am interested in ${person.hobbies[0]} and ${person.hobbies[1]`}):
+// const person = {
+//     name:['Bob', 'Smith'],
+//     age:32,
+//     gender: 'male',
+//     interests: ['music', 'skiing']
+// };
+// console.log(`My name is ${person.name[0]} ${person.name[1]}. I am interested in ${person.hobbies[0]} and ${person.hobbies[1]`}):
 
-for (let key in person)
+// for (let key in person)
+// {
+//     console.log(key);
+//     console.log(person[key]);
+// }
+
+let student1 = {
+    ID: "A00022",
+    GPA: 3.0,
+    program: "CIT"
+    };
+    let student2 = {
+    ID: "A01000",
+    GPA: 3.1,
+    program: "CST"
+    };
+    let student3 = {
+    ID: "A00114",
+    GPA: 3.2,
+    program: "CIT"
+    };
+    let students = [student1, student2, student3];
+    let counter = 0;
+
+for (let student of students)
 {
-    console.log(key);
-    console.log(person[key]);
+    if (student.program === "CIT" && student.GPA > 3)
+    {
+        counter++;
+        console.log(student);
+    }
 }
+console.log(counter);
