f9c255a9dd694e884faee668cb0d0fa659cc749c
Andrew Yoon <yoonandrew416@gmail.com>
2022-11-08 16:00:28 -0800

    Array of Objects

--- scripts/script.js
+++ scripts/script.js
@@ -19,15 +19,39 @@
 // while(!isNaN(Number(input)));
 // alert("Hello " + input);
 
-let fruits=["Apple", "Banana"];
+// let fruits=["Apple", "Banana"];
+// let pushResult = fruits.push("Orange");
+// console.log(pushResult);
+
+// fruits.pop();
+// console.log(fruits);
+
+// fruits.unshift("Mango");
+// console.log(fruits);
+
+// fruits.shift();
+// console.log(fruits);
+
+// console.log(fruits.indexOf("Apple"))
+
+let fruits =["Apple", "Banana"];
 fruits.push("Orange");
-console.log(fruits);
+let arr = [1,2,3,4,5,6,7,8,9,10]
+for (let item of arr)
+{
+    console.log(2 ** item);
+}
 
-fruits.pop();
-console.log(fruits);
+const person = {
+    name:['Bob', 'Smith'],
+    age:32,
+    gender: 'male',
+    interests: ['music', 'skiing']
+};
+console.log(`My name is ${person.name[0]} ${person.name[1]}. I am interested in ${person.hobbies[0]} and ${person.hobbies[1]`}):
 
-fruits.unshift("Mango");
-console.log(fruits);
-
-fruits.shift();
-console.log(fruits);
+for (let key in person)
+{
+    console.log(key);
+    console.log(person[key]);
+}
